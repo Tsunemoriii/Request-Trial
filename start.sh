c729e715d7f4761a7eb0771f200495f3921f25fb
MahirModan <107642456+MahirModan@users.noreply.github.com>
2023-06-20 23:09:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MahirModan/HAFB.git /HAFB
+  git clone https://github.com/MahirModan/HAFB /HAFB
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /HAFB

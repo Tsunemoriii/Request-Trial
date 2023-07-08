572d2eb65e823ba9395cbc5773ed88e827bc66bb
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-07-08 08:42:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MahirModan/utah.git /utah
+  git clone https://github.com/Tsunemoriii/Request.git /Request
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/MahirModan/utah /utah
+  git clone https://github.com/Tsunemoriii/Request /Request
 fi
-cd /utah
+cd /Request
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

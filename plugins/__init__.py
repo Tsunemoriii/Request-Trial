14c9cd007bc654659c014f2edc09744448c2bf33
Captain Ezio <iamgojoof6eyes@gmail.com>
2023-07-09 20:28:26 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,7 +2,7 @@ from aiohttp import web
 from .route import routes
 from info import AUTO_DEL
 from datetime import datetime, timedelta
-from bot import app
+#from bot import app
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
@@ -19,12 +19,3 @@ def till_date(date):
     z = datetime.strptime(date,form)
     return z
 
-from database.auto_del_mess import auto_del_get, auto_del_delete
-
-while True:
-    z = auto_del_get()
-    for i in z:
-        tim = till_date(i["datee"])
-        if tim == datetime.now():
-            app.delete_messages(i["chat_id"],i["mess_id"])
-            auto_del_delete(i["datee"], i["chat_id"], i["mess_id"])

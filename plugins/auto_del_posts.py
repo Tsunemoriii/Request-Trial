3c5408f301bd67c35c9d03ebed475c5bf906af41
Captain Ezio <iamgojoof6eyes@gmail.com>
2023-07-09 20:29:33 +0530

    Create auto_del_posts.py

---
+++ plugins/auto_del_posts.py
@@ -0,0 +1,18 @@
+from info import AUTO_DEL
+from datetime import datetime, timedelta
+from bot import app
+
+def till_date(date):
+    form = "%Y-%m-%d %H:%M:%S.%f"
+    z = datetime.strptime(date,form)
+    return z
+
+from database.auto_del_mess import auto_del_get, auto_del_delete
+
+while True:
+    z = auto_del_get()
+    for i in z:
+        tim = till_date(i["datee"])
+        if tim == datetime.now():
+            app.delete_messages(i["chat_id"],i["mess_id"])
+            auto_del_delete(i["datee"], i["chat_id"], i["mess_id"])

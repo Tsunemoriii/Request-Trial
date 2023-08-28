ceb3a6cd11586a2adb6e4823c73ebd4e0a669eae
Captain Ezio <iamgojoof6eyes@gmail.com>
2023-08-28 15:00:17 +0530

    Update auto_del_posts.py

--- plugins/auto_del_posts.py
+++ plugins/auto_del_posts.py
@@ -7,8 +7,13 @@ from pyrogram import Client
 # from bot import app importing app gives err have to find alt
 
 def till_date(date):
-    form = "%Y-%m-%d %H:%M:%S.%f"
-    z = datetime.strptime(date,form)
+    try:
+        form = "%Y-%m-%d %H:%M:%S.%f"
+        z = datetime.strptime(date,form)
+    except ValueError:
+        date = date.rsplit(".",1)[0]
+        form = "%Y-%m-%d %H:%M:%S"
+        z = datetime.strptime(date,form)
     return z
 
 from database.auto_del_mess import auto_del_delete, auto_del_get
